5e4041738442b496f3b102b8d0052225ddc7ff5d
trungld3 <k21tpm4@gmail.com>
2020-05-18 15:36:53 +0700

    complete

--- Table View test/ViewController.swift
+++ Table View test/ViewController.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = String (indexPath.row + 1)
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
